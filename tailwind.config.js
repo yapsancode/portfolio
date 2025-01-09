96296a82103827a01804c73cff31d36491d3b033
Isyraf <isyrafsan97@gmail.com>
2025-01-10 00:44:09 +0800

    modify the sidebar with some animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,35 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  mode: 'jit',
-  purge: [
-    './src/**/*.{js,jsx,ts,tsx}',
-    './public/index.html',
-    './src/**/*.{js,jsx,ts,tsx}',
-  ],
-  darkMode: false, // or 'media' or 'class'
-  content: [],
-  theme: {
-    extend: {},
+import colors, { gray } from 'tailwindcss/colors'
+export const mode = 'jit'
+export const purge = [
+  './src/**/*.{js,jsx,ts,tsx}',
+  './public/index.html',
+  './src/**/*.{js,jsx,ts,tsx}',
+]
+export const darkMode = false
+export const content = []
+export const theme = {
+  extend: {
+    colors: {
+      primary: '#202225',
+      secondary: '#5865F2',
+      gray: colors.gray,
+      gray: {
+        900: '#202225',
+        800: '#2f3136',
+        700: '#36393f',
+        600: '#4f545c',
+        500: '#72767d',
+        400: '#a3a6aa',
+        300: '#dcddde',
+        200: '#ebedef',
+        100: '#f2f3f5'
+      }
+    },
   },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
 }
+export const variants = {
+  extend: {},
+}
+export const plugins = []
 
